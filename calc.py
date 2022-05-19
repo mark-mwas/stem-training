84140dd0c4e69b24e8a553ffefc7a92fae8cf7ff
leviiii <marklevin034@gmail.com>
2022-05-19 12:49:26 +0300

    strings in python

---
+++ calc.py
@@ -0,0 +1,9 @@
+f_num =input("Enter the first number :")
+s_num =input("Enter the second number :")
+
+sum_of_numbers = f_num + s_num
+print("The sum of the two numbers is"+str(sum_of_nums)) 
+sum_of_numbers = f_num + s_num
+print("The sum of the two numbers is"+str(sum_of_nums))
+sum_of_nums = f_num + s_num
+print("The sum of the two numbers is +sum_of_nums"=int(sum_of_nums))s
---
+++ strings.py
@@ -0,0 +1,32 @@
+name = "levinmark"
+
+# Multiple line strings
+msg = """i am levin.i am a boy"""
+
+city ="nairobi"
+# .upper() to convert to upper case
+print(city.upper())
+
+uni ="JKUAT"
+print(uni.lower())
+
+fruit ="pineapple"
+
+print(fruit[:2])
+print(fruit[5:])
+print(fruit[-1])
+
+
+f_name = " marklevin"
+print(f_name.strip())
+
+s_name ="levin"
+#concatination
+full_name =f_name+s_name
+print(full_name)
+
+city ="mombasa"
+print(len(city))
+
+name ="abracadabra"
+print(len(name))
